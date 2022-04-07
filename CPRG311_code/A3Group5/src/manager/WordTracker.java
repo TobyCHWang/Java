2069680af0434d747fd2f9fddeb39f422529a5c9
toby2235 <watertoby@gmail.com>
2022-04-06 20:45:00 -0600

    040622

--- CPRG311_code/A3Group5/src/manager/WordTracker.java
+++ CPRG311_code/A3Group5/src/manager/WordTracker.java
@@ -1,5 +1,30 @@
 package manager;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class WordTracker {
+	
+	public WordTracker() throws IOException {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter file location: ");
+		String fileLoc = input.nextLine();
+		loadFile(fileLoc);
+	}
+
+	private void loadFile(String fileLoc) throws IOException {
+		File file = new File(fileLoc);
+		BufferedReader fin = new BufferedReader(new FileReader(file));
+		String line;
+
+		while ((line = fin.readLine()) != null) {
+            System.out.println(line);
+        }
+		
+	}
 
 }
